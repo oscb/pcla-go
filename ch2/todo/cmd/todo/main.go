e256d3113b67ef2100acac50e1799b9b6e0acadb
Oscar Bazaldua <511911+oscb@users.noreply.github.com>
2024-01-17 17:49:11 -0800

    ch2: todo app v2

--- ch2/todo/cmd/todo/main.go
+++ ch2/todo/cmd/todo/main.go
@@ -2,35 +2,63 @@ package main
 
 import (
 	"ch2/todo"
+	"flag"
 	"fmt"
 	"os"
-	"strings"
 )
 
 const todoFileName = ".todo.json"
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "%s tool. Developed for The Pragmatic Bookshelf\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2024\n")
+		fmt.Fprintln(flag.CommandLine.Output(), "Usage information:")
+		flag.PrintDefaults()
+	}
+	// Parsing command line flags
+	task := flag.String("task", "", "Task to be included in the todo list")
+	list := flag.Bool("list", false, "List all the tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+	flag.Parse()
+
+	// Initialize the todo list from file
 	l := &todo.List{}
 	if err := l.Get(todoFileName); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	// Decide what to do based on the number of arguments provided
-	switch {
-	// For no arguments, print the list
-	case len(os.Args) == 1:
-		for _, item := range *l {
-			fmt.Println(item.Task)
-		}
-	// Concatenate all provided arguments with a space and add to the list as an item
-	default:
-		item := strings.Join(os.Args[1:], " ")
-		l.Add(item)
-
+	saveList := func() {
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+	}
+
+	// Decide what to do based on the flags
+	switch {
+	// Show list
+	case *list:
+		for _, item := range *l {
+			if !item.Done {
+				fmt.Println(item)
+			}
+		}
+	// Complete item
+	case *complete > 0:
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		saveList()
+	// Add task
+	case *task != "":
+		l.Add(*task)
+		saveList()
+	// Invalid flag
+	default:
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
 	}
 }
