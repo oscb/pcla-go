aba24da5315f28fd4955763b3b963e4afd5b721d
Oscar Bazaldua <511911+oscb@users.noreply.github.com>
2024-01-18 19:05:58 -0800

    ch3: mdp v2

--- ch3/mdp/main.go
+++ ch3/mdp/main.go
@@ -4,8 +4,11 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"os"
-	"path/filepath"
+	"os/exec"
+	"runtime"
+	"time"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
@@ -28,6 +31,7 @@ const (
 
 func main() {
 	filename := flag.String("file", "", "Markdown file to preview")
+	skip := flag.Bool("s", false, "Skip Preview")
 	flag.Parse()
 
 	if *filename == "" {
@@ -35,13 +39,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := run(*filename); err != nil {
+	if err := run(*filename, os.Stdout, *skip); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-func run(filename string) error {
+func run(filename string, out io.Writer, skipPreview bool) error {
 	// Read all teh data from the input file and check for errors
 	input, err := os.ReadFile(filename)
 	if err != nil {
@@ -50,10 +54,29 @@ func run(filename string) error {
 
 	htmlData := parseContent(input)
 
-	outName := fmt.Sprintf("%s.html", filepath.Base(filename))
-	fmt.Println(outName)
+	// Create temporary file and check for errors
+	temp, err := os.CreateTemp("", "mdp*.html")
+	if err != nil {
+		return err
+	}
+	if err := temp.Close(); err != nil {
+		return err
+	}
+	outName := temp.Name()
 
-	return saveHTML(outName, htmlData)
+	fmt.Fprintln(out, outName)
+
+	if err := saveHTML(outName, htmlData); err != nil {
+		return err
+	}
+
+	if skipPreview {
+		return nil
+	}
+
+	defer os.Remove(outName)
+
+	return preview(outName)
 }
 
 func parseContent(input []byte) []byte {
@@ -74,4 +97,39 @@ func parseContent(input []byte) []byte {
 func saveHTML(outFname string, data []byte) error {
 	// Write the bytes to the file
 	return os.WriteFile(outFname, data, 0644)
+}
+
+func preview(fname string) error {
+	cName := ""
+	cParams := []string{}
+
+	// define executable based on OS
+	switch runtime.GOOS {
+	case "linux":
+		cName = "xdg-open"
+	case "windows":
+		cName = "cmd.exe"
+		cParams = []string{"/C", "start"}
+	case "darwin":
+		cName = "open"
+	default:
+		return fmt.Errorf("OS Not supported")
+	}
+
+	// Append filename to parameters slice
+	cParams = append(cParams, fname)
+
+	// Locate executable in PATH
+	cPath, err := exec.LookPath(cName)
+	if err != nil {
+		return err
+	}
+
+	// Open the file with the default program
+	err = exec.Command(cPath, cParams...).Run()
+
+	// Give the browser time to open the file before deletion,
+	// this is a very naive fix for race conditions
+	time.Sleep(2 * time.Second)
+	return err
 }
