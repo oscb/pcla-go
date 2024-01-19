4167ff1c8e083a50bdef5cece87858d6e3fecfbe
Oscar Bazaldua <511911+oscb@users.noreply.github.com>
2024-01-18 18:35:36 -0800

    ch3: mdp v1

---
+++ ch3/mdp/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
+)
+
+const (
+	header = `<!DOCTYPE html>
+	<html>
+		<head>
+			<meta http-equiv="content-type" content="text/html;charset=utf-8">
+			<title>Markdown Preview Tool</title>
+		</head>
+		<body>
+	`
+	footer = `
+		</body>
+	</html>
+	`
+)
+
+func main() {
+	filename := flag.String("file", "", "Markdown file to preview")
+	flag.Parse()
+
+	if *filename == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if err := run(*filename); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(filename string) error {
+	// Read all teh data from the input file and check for errors
+	input, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	htmlData := parseContent(input)
+
+	outName := fmt.Sprintf("%s.html", filepath.Base(filename))
+	fmt.Println(outName)
+
+	return saveHTML(outName, htmlData)
+}
+
+func parseContent(input []byte) []byte {
+	// Parse the markdown file through blackfriday and bluemonday
+	// to generate a valid and safe HTML
+	output := blackfriday.Run(input)
+	body := bluemonday.UGCPolicy().SanitizeBytes(output)
+
+	// Create a buffer of bytes to write to file
+	var buffer bytes.Buffer
+	buffer.WriteString(header)
+	buffer.Write(body)
+	buffer.WriteString(footer)
+
+	return buffer.Bytes()
+}
+
+func saveHTML(outFname string, data []byte) error {
+	// Write the bytes to the file
+	return os.WriteFile(outFname, data, 0644)
+}
---
+++ ch3/mdp/testdata/test1.md
@@ -0,0 +1,11 @@
+# Test Markdown File
+
+Just a test
+
+## Bullets:
+* Links [Link1](https://example)
+
+## Code Block
+```
+some code
+```
